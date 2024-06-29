8b1148b51cfd7a593b2f21800e487f04a785e41b
Shawn Koski <shawnk@aplos.com>
2024-06-29 10:56:23 -0700

    remove action function and createTodo function

--- src/components/TodoComposer/TodoComposer.tsx
+++ src/components/TodoComposer/TodoComposer.tsx
@@ -2,31 +2,6 @@ import React from 'react';
 import { Todo } from '../../types';
 import { Form } from 'react-router-dom';
 
-export async function action({ request }: { request: Request }) {
-  console.log('todo POST action - request', request);
-
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const title = data.title as string;
-  const todo = await createTodo(title);
-  return { todo };
-}
-
-async function createTodo(title: string): Promise<Todo> {
-  const response = await fetch('http://localhost:3000/todos', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ title })
-  });
-  if (!response.ok) {
-    throw new Error('Failed to create todo');
-  }
-  const todo = await response.json();
-  return todo;
-}
-
 function TodoComposer() {
   const [title, setTitle] = React.useState('');
 
@@ -39,7 +14,9 @@ function TodoComposer() {
         onChange={(e) => setTitle(e.target.value)}
         className="border-black border"
       />
-      <button type="submit">Add Todo</button>
+      <button type="submit" name="intent" value="create">
+        Add Todo
+      </button>
     </Form>
   );
 }
